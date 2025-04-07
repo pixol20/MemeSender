71fbd2eb3abd4e737e45118145a0963ab5faf5ae
pixol20 <115364463+pixol20@users.noreply.github.com>
2025-04-07 15:55:28 +0300

    make constants final

--- bot_backend/src/constants.py
+++ bot_backend/src/constants.py
@@ -1,13 +1,14 @@
+from typing import Final
 # user_data keys
-MEME_NAME = "meme_name"
-MEDIA_TYPE = "media_type"
-TELEGRAM_MEDIA_ID = "telegram_media_id"
-DURATION = "duration"
-TAGS = "tags"
-MEME_PUBLIC = "meme_public"
+MEME_NAME: Final[str] = "meme_name"
+MEDIA_TYPE: Final[str]  = "media_type"
+TELEGRAM_MEDIA_ID: Final[str]  = "telegram_media_id"
+DURATION: Final[str]  = "duration"
+TAGS: Final[str]  = "tags"
+MEME_PUBLIC: Final[str]  = "meme_public"
 
 # Message from bot that contains memes created by user.
-MEMES_CONTROL_MESSAGE = "memes_control_message"
-MEME_MEDIA_MESSAGE = "meme_media_message"
+MEMES_CONTROL_MESSAGE: Final[str]  = "memes_control_message"
+MEME_MEDIA_MESSAGE: Final[str]  = "meme_media_message"
 
-LAST_SELECTED_PAGE = "last_selected_page"
+LAST_SELECTED_PAGE: Final[str]  = "last_selected_page"
